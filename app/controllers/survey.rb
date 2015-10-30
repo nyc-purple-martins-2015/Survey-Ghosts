b59c3247a02797a02a40a794c137dabe204a9175
Amy Liao <amyhliao88@gmail.com>
2015-10-30 15:02:28 -0700

    create path to create new survey

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -3,3 +3,18 @@ get "/surveys" do
   # @author =
   erb :"/surveys/index"
 end
+
+get '/surveys/new' do
+  erb :'surveys/new'
+end
+
+post '/surveys' do
+  user = current_user
+  survey = Survey.new(params[:survey])
+  if survey.save
+    redirect "/"
+  else
+    @errors = survey.errors.full_messages
+    erb :'surveys/new'
+  end
+end
