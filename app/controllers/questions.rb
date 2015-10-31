e52351449e7efce18488cf683b59500dd6d96cae
Amy Liao <amyhliao88@gmail.com>
2015-10-30 17:01:17 -0700

    add path to add new questions to survey

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -5,3 +5,35 @@ get '/surveys/:id/questions/new' do
   @choices = []
   erb :'questions/new'
 end
+
+post '/surveys/:id/questions' do
+  survey = Survey.find_by(id: params[:id])
+  question = Question.new(survey: survey, query: params[:query])
+  options = []
+  errors = []
+  if question.save
+    params[:options].each do |_, option_text|
+      if option_text.strip != ""
+        option = Option.new(question: question, response: option_text.strip)
+        if option.save
+          options << option
+        else
+          errors += option.errors.full_messages
+        end
+      end
+    end
+  else
+    errors += question.errors.full_messages
+  end
+  if options.count < 2
+    errors << "You must add at least two options"
+  end
+  if !errors.empty?
+    options.each { |option| option.destroy }
+    question.destroy
+    p errors
+    redirect :"/surveys/#{survey.id}/questions/new?#{params.to_query}"
+  else
+    redirect :"/surveys/#{survey.id}/questions/new"
+  end
+end
